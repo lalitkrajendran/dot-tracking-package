54d66ce527cdd8cb887ba05180e5075cfcb3f970
Lalit K. Rajendran <lrajendr@purdue.edu>
2019-07-10 20:54:36 -0400

    changed the sample code to run camera calibration to a more recent example

--- run_camera_calibration_example.m
+++ run_camera_calibration_example.m
@@ -6,29 +6,74 @@ clc
 
 restoredefaultpath;
 addpath ../dot-tracking-package/
+addpath /scratch/shannon/c/aether/Projects/BOS/general-codes/matlab-codes/
 
 %% experiment settings
-test_date = '2019-03-14';
-% body placed in the tunnel
-body = 'wedge';
-% dot size on the target (mm)
-dot_size = 0.15;
-dot_size_string = num2str(['0_' num2str(dot_size*100) 'mm']);
-% top level project directory
-project_directory = '/scratch/shannon/c/aether/Projects/BOS/arms-supersonic-wind-tunnel/';
-% top directory containing images for this case
-top_image_directory = fullfile(project_directory, 'analysis', 'data', test_date, body, dot_size_string); 
-% top directory to contain results for this case
-top_results_directory = fullfile(project_directory, 'analysis', 'results', test_date, body, dot_size_string);
 
-%% call calibration code
-calibration_image_directory = fullfile(top_image_directory, 'calibration-crlb');
+% date on which the test was conducted
+date = '2019-03-22';
+% electrode gap (mm)
+electrode_gap_array = [2, 5];
+% imaging views
+views = {'front-view'; 'side-view'; 'top-view'};
+
+%% read/write settings
+
+% top directory containing images
+top_image_directory = '/scratch/shannon/c/aether/Projects/plasma-induced-flow/analysis/data/dbd/';
+% top directory containing results
+top_results_directory = '/scratch/shannon/c/aether/Projects/plasma-induced-flow/analysis/results/dbd/';
+
+%% calibration settings
+% dot size (mm)
+dot_size = 0.042;
+dot_diameter = dot_size * 1e-3;
+dot_spacing = 2 * dot_size * 1e-3;
+dot_skip = 2;
+order_z = 1;
 save_calibration_data = true;
 save_camera_model = true;
 read_calibration_data = false;
-dot_diameter = dot_size * 1e-3;
-dot_spacing = 2 * dot_size * 1e-3;
-dot_skip = 3;
-order_z = 1;
+%% run calibration
 
-calculate_camera_model_coefficients_reference_image_02(calibration_image_directory, save_calibration_data, save_camera_model, read_calibration_data, dot_diameter, dot_spacing, dot_skip, order_z);
+% =====================================
+% loop through electrode gaps
+% =====================================
+for electrode_gap = electrode_gap_array
+    fprintf('Gap: %d mm\n', electrode_gap);
+    current_gap_directory = fullfile(top_image_directory, date, [num2str(electrode_gap) 'mm']);
+    % =====================================
+    % loop through views
+    % =====================================
+    for view_index = 1:numel(views)
+        fprintf('%s\n', views{view_index});
+        current_view_directory = fullfile(current_gap_directory, views{view_index});
+        % get list of folders containing different pulse parameters
+        pulse_parameter_folders = dir(fullfile(current_view_directory, 'bos', '*V*ns'));
+        % load mask
+        mask = imread(fullfile(current_view_directory, 'bos', 'mask.tif'));
+        % =====================================
+        % loop through pulse parameters
+        % =====================================
+        for pulse_parameter_index = 1:length(pulse_parameter_folders)
+            fprintf('case: %s\n', pulse_parameter_folders(pulse_parameter_index).name);
+            current_pulse_parameter_directory = fullfile(current_view_directory, 'bos', pulse_parameter_folders(pulse_parameter_index).name);
+            
+            % create calibration directory
+            calibration_image_directory = fullfile(current_pulse_parameter_directory, 'calibration');
+            if ~exist(calibration_image_directory, 'dir')
+                mkdir(calibration_image_directory);
+            end
+            
+            % load sample reference image to be used for the calibration
+            im_ref = imread(fullfile(current_pulse_parameter_directory, 'shot01', 'ref', 'im__0001.tif'));
+            % mask image
+            im_ref_masked = create_masked_image(im_ref, mask);
+            % save masked image in the calibration folder
+            imwrite(im_ref_masked, fullfile(calibration_image_directory, 'im.tif'));
+            
+            % call calibration code
+            calculate_camera_model_coefficients_reference_image_02(calibration_image_directory, save_calibration_data, save_camera_model, read_calibration_data, dot_diameter, dot_spacing, dot_skip, order_z);
+        end
+    end
+end
